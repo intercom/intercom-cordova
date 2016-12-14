d8a47bd9c2b2997f325fbc01da6dce5c32b13ccb
Conor O'Donnell <conor.odonnell@intercom.io>
2016-12-14 11:23:20 +0000

    Bump version and Android version

--- src/ios/IntercomBridge.m
+++ src/ios/IntercomBridge.m
@@ -9,7 +9,7 @@
 @implementation IntercomBridge : CDVPlugin
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"3.0.20"];
+    [Intercom setCordovaVersion:@"3.0.21"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
