f85a9c79e8206dee7dd4b50a193ed810115eb08f
James Treanor <james@intercom.io>
2016-12-19 15:51:30 +0000

    Release version 3.0.22

--- src/ios/IntercomBridge.m
+++ src/ios/IntercomBridge.m
@@ -9,7 +9,7 @@
 @implementation IntercomBridge : CDVPlugin
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"3.0.21"];
+    [Intercom setCordovaVersion:@"3.0.22"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
