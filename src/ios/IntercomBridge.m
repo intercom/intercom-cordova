b87fd65b2068f74c757d3fa299a026883ec7e780
Brian Boyle <bboyle18@gmail.com>
2016-12-22 14:45:14 +0000

    Release 3.0.23 - Added support for Ionic 2

--- src/ios/IntercomBridge.m
+++ src/ios/IntercomBridge.m
@@ -9,7 +9,7 @@
 @implementation IntercomBridge : CDVPlugin
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"3.0.22"];
+    [Intercom setCordovaVersion:@"3.0.23"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
