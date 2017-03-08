a37d8b71507552f5f682447c737eb14697ee9f90
James Treanor <james@intercom.io>
2017-03-08 16:45:02 +0000

    Added setupFcm.js hook to enable using 'google-services.json'

---
+++ src/android/setupFcm.js
@@ -0,0 +1,15 @@
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        deferral = ctx.requireCordovaModule('q').defer();
+
+    var settingsFile = path.join(ctx.opts.projectRoot, 'google-services.json');
+    fs.stat(settingsFile, function(err,stats) {
+        if (err) {
+            console.log("If you wish to use FCM push notifications with Intercom, you'll need to add a google-services.json file with the FCM project_info and place that into your project's root folder");
+        } else {
+            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
+        }
+    });
+    return deferral.promise;
+};
