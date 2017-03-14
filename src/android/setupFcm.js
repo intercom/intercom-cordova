4d4a155a4fd489dd9b68cd0fb9570c57d315d69f
James Treanor <james@intercom.io>
2017-03-14 14:47:15 +0000

    Simplified FCM implementation (#152)

--- src/android/setupFcm.js
+++ src/android/setupFcm.js
@@ -17,27 +17,8 @@ module.exports = function(ctx) {
             console.log("Intercom: If you wish to use FCM push notifications, you'll need to add a google-services.json file with the FCM project_info and place that into your project's root folder");
             deferral.resolve();
         } else {
-            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
-
-            var lineReader = readline.createInterface({
-                terminal: false,
-                input : fs.createReadStream('platforms/android/build.gradle')
-            });
-            var classPathCount = 0;
-            lineReader.on("line", function(line) {
-                fs.appendFileSync('./build.gradle.tmp', line.toString() + os.EOL);
-                if (/.*\ dependencies \{.*/.test(line)) {
-                    fs.appendFileSync('./build.gradle.tmp', '\t\tclasspath "com.google.gms:google-services:3.0.0"' + os.EOL);
-                } else if (/.*classpath( |\t)+(\"|')com.google.gms:google-services:.*/.test(line)) {
-                    classPathCount = classPathCount + 1;
-                }
-            }).on("close", function () {
-                if (classPathCount == 0) {
-                    fs.rename('./build.gradle.tmp', 'platforms/android/build.gradle', deferral.resolve);
-                } else {
-                    fs.unlink('./build.gradle.tmp', deferral.resolve)
-                }
-            });
+            var stream = fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
+            stream.on('finish', deferral.resolve);
         }
     });
     return deferral.promise;
