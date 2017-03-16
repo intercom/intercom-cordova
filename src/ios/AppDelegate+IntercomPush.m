8d01ec614c212cb987eb8673bac0e6e09378fc60
James Treanor <james@intercom.io>
2017-03-16 16:48:03 +0000

    Push plugin compatibility (#156)

--- src/ios/AppDelegate+IntercomPush.m
+++ src/ios/AppDelegate+IntercomPush.m
@@ -5,49 +5,75 @@
 @implementation AppDelegate (IntercomPush)
 
 + (void)load {
-    [self swizzleDidRegisterForRemoteNotification];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzleDidRegisterForRemoteNotification];
+        [self swizzlePushPlugin];
+    });
 }
 
-static void (*OriginalDidRegisterForRemoteNotification)(id, SEL, UIApplication *, NSData *);
 + (void)swizzleDidRegisterForRemoteNotification {
-    //Initial class to swizzle
     __block Class originalClass = [AppDelegate class];
     if (!originalClass) {
         return;
     }
     
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        SEL originalSelector = @selector(application:didRegisterForRemoteNotificationsWithDeviceToken:);
-
-        //Replacement IMP for original method
-        IMP replacement = imp_implementationWithBlock(^void (id _self, UIApplication *application, NSData *deviceToken) {
-            [Intercom setDeviceToken:deviceToken];
-            
-            //Call to original method
-            if (*OriginalDidRegisterForRemoteNotification != NULL){
-                OriginalDidRegisterForRemoteNotification(_self, _cmd, application, deviceToken);
-            }
-        });
-
-        //Swizzling
-        IMP *store = (IMP *) &OriginalDidRegisterForRemoteNotification;
-        IMP originalImp = NULL;
-        Method method = class_getInstanceMethod(originalClass, originalSelector);
-        if (method) {
-            const char *type = method_getTypeEncoding(method);
-            originalImp = class_replaceMethod(originalClass, originalSelector, replacement, type);
-            if (!originalImp) {
-                originalImp = method_getImplementation(method);
-            }
-            if (originalImp && store) {
-                *store = originalImp;
-            }
-        }else{
-            *store = NULL;
-            class_addMethod(originalClass, originalSelector, replacement, "v@:@@");
+    SEL originalSelector = @selector(application:didRegisterForRemoteNotificationsWithDeviceToken:);
+    __block IMP originalImp = NULL;
+    
+    //Replacement IMP for original method
+    IMP replacement = imp_implementationWithBlock(^void (id _self, UIApplication *application, NSData *deviceToken) {
+        [Intercom setDeviceToken:deviceToken];
+        
+        //perform host app push logic here
+        if (originalImp != NULL) {
+            ((void(*)(id, SEL, UIApplication *, NSData *))originalImp)(_self, _cmd, application, deviceToken);
         }
     });
+    
+    //Swizzling
+    Method method = class_getInstanceMethod(originalClass, originalSelector);
+    if (method) {
+        const char *type = method_getTypeEncoding(method);
+        originalImp = class_replaceMethod(originalClass, originalSelector, replacement, type);
+        if (!originalImp) {
+            originalImp = method_getImplementation(method);
+        }
+    } else {
+        class_addMethod(originalClass, originalSelector, replacement, "v@:@@");
+    }
 }
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
++ (void)swizzlePushPlugin {
+    __block Class originalClass = [self class];
+    if (!originalClass) {
+        return;
+    }
+    
+    SEL originalSelector = @selector(setLaunchNotification:);
+    __block IMP originalImp = NULL;
+    
+    //Replacement IMP for original method
+    IMP replacement = imp_implementationWithBlock(^void (id _self, NSDictionary *launchNotification) {
+        if (launchNotification && [Intercom isIntercomPushNotification:launchNotification]) {
+            [Intercom handleIntercomPushNotification:launchNotification];
+        } else if (originalImp != NULL) {
+            ((void(*)(id, SEL, NSDictionary *))originalImp)(_self, _cmd, launchNotification);
+        }
+    });
+    
+    //Swizzling
+    Method method = class_getInstanceMethod(originalClass, originalSelector);
+    if (method) {
+        const char *type = method_getTypeEncoding(method);
+        originalImp = class_replaceMethod(originalClass, originalSelector, replacement, type);
+        if (!originalImp) {
+            originalImp = method_getImplementation(method);
+        }
+    }
+}
+#pragma clang diagnostic pop
 
 @end
