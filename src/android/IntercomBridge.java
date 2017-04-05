0d24d2a67ca7ae8f9c1634e313c791391f3ed947
James Treanor <james@intercom.io>
2017-04-05 13:21:34 +0100

    Release 3.1.3

--- src/android/IntercomBridge.java
+++ src/android/IntercomBridge.java
@@ -66,7 +66,7 @@ public class IntercomBridge extends CordovaPlugin {
         try {
             Context context = IntercomBridge.this.cordova.getActivity().getApplicationContext();
 
-            CordovaHeaderInterceptor.setCordovaVersion(context, "3.1.2");
+            CordovaHeaderInterceptor.setCordovaVersion(context, "3.1.3");
 
             switch (IntercomPushManager.getInstalledModuleType()) {
                 case GCM: {
