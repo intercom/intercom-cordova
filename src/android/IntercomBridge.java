c120d45f41fb8cdc8f5c8879de2a84c71c1dce7f
James Treanor <james@intercom.io>
2017-08-25 16:28:13 +0100

    Bump version for 4.0.0

--- src/android/IntercomBridge.java
+++ src/android/IntercomBridge.java
@@ -66,7 +66,7 @@ public class IntercomBridge extends CordovaPlugin {
         try {
             Context context = IntercomBridge.this.cordova.getActivity().getApplicationContext();
 
-            CordovaHeaderInterceptor.setCordovaVersion(context, "3.2.2");
+            CordovaHeaderInterceptor.setCordovaVersion(context, "4.0.0");
 
             switch (IntercomPushManager.getInstalledModuleType()) {
                 case GCM: {
--- src/ios/IntercomBridge.m
+++ src/ios/IntercomBridge.m
@@ -9,7 +9,7 @@
 @implementation IntercomBridge : CDVPlugin
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"3.2.2"];
+    [Intercom setCordovaVersion:@"4.0.0"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
