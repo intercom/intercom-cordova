359e3af129c4547d636dc81ba904395b7bc42873
Conor O'Donnell <mrconor@gmail.com>
2018-02-12 15:49:18 +0000

    Bump version to 5.0.2

--- intercom-plugin/src/ios/IntercomBridge.m
+++ intercom-plugin/src/ios/IntercomBridge.m
@@ -9,7 +9,7 @@
 @implementation IntercomBridge : CDVPlugin
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"5.0.1"];
+    [Intercom setCordovaVersion:@"5.0.2"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
