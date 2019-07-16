99354f569bb7c055ea4d97fdfaaf25c6d68e05d5
nymerias <nicolette.yadegar@intercom.io>
2019-07-15 17:11:50 -0700

    update Cordova version in bridge

--- intercom-plugin/src/android/IntercomBridge.java
+++ intercom-plugin/src/android/IntercomBridge.java
@@ -60,7 +60,7 @@ public class IntercomBridge extends CordovaPlugin {
         try {
             Context context = cordova.getActivity().getApplicationContext();
 
-            CordovaHeaderInterceptor.setCordovaVersion(context, "6.2.0");
+            CordovaHeaderInterceptor.setCordovaVersion(context, "7.0.0");
 
             switch (IntercomPushManager.getInstalledModuleType()) {
                 case FCM: {
