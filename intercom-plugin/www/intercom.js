7a7c5a1e5c1dc88803173a8144fd799d39f205f1
Brian Boyle <brian.boyle@intercom.io>
2022-09-12 20:35:54 +0100

    iOS Support
    - Added support for iOS 16
    - Removed deprecated methods

--- intercom-plugin/www/intercom.js
+++ intercom-plugin/www/intercom.js
@@ -7,16 +7,8 @@ var intercom = {
         cordova.exec(success, error, 'Intercom', 'registerUnidentifiedUser', []);
     },
 
-    reset: function(success, error) {
-        cordova.exec(success, error, 'Intercom', 'logout', []);
-    },
-
     logout: function(success, error) {
         cordova.exec(success, error, 'Intercom', 'logout', []);
-    },
-
-    setSecureMode: function(secureHash, secureData, success, error) {
-        cordova.exec(success, error, 'Intercom', 'setSecureMode', [secureHash, secureData]);
     },
 
     setUserHash: function(secureHash, success, error) {
@@ -35,16 +27,8 @@ var intercom = {
         cordova.exec(success, error, 'Intercom', 'displayMessenger', []);
     },
 
-    displayMessageComposer: function(success, error) {
-        cordova.exec(success, error, 'Intercom', 'displayMessageComposer', []);
-    },
-
-    displayMessageComposerWithInitialMessage: function(initialMessage, success, error) {
-        cordova.exec(success, error, 'Intercom', 'displayMessageComposerWithInitialMessage', [initialMessage]);
-    },
-
-    displayConversationsList: function(success, error) {
-        cordova.exec(success, error, 'Intercom', 'displayConversationsList', []);
+    displayMessageComposer: function(initialMessage, success, error) {
+        cordova.exec(success, error, 'Intercom', 'displayMessageComposer', [initialMessage]);
     },
 
     displayHelpCenter: function(success, error) {
