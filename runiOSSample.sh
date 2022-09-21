68d62635b58c4ebc689e67095d925b90f520ddd2
jb saouzanet <jean-baptiste.saouzanet@checkproof.com>
2022-09-21 11:50:15 +0200

    Re-organizing forked project tag 12.1.0

--- runiOSSample.sh
+++
@@ -1,7 +0,0 @@
-clear
-cd Example
-cordova plugin remove ../intercom-plugin
-cordova plugin add ../intercom-plugin
-cordova platform remove ios
-cordova platform add ios --save
-cordova run ios --stacktrace
