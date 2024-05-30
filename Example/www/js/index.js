3e00a773d5d12331df8854ecc281eb2b4a560d38
Brian Boyle <brian.boyle@intercom.io>
2024-05-30 14:11:12 +0100

    android changes

--- Example/www/js/index.js
+++ Example/www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-
+        console.log('hello');
         intercom.setLauncherVisibility('VISIBLE');
         intercom.setInAppMessageVisibility('VISIBLE');
 
--- runAndroidSample.sh
+++ runAndroidSample.sh
@@ -7,5 +7,5 @@ cd Example
 cordova plugin remove ../intercom-plugin
 cordova plugin add ../intercom-plugin
 cordova platform remove android
-cordova platform add android@10.1.1 --save
+cordova platform add android@13.0.0 --save
 cordova run android --stacktrace
