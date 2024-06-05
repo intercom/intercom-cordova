ba17ebe09fe5f3a72e0c27a580097158910d1b63
Brian Boyle <brian.boyle@intercom.io>
2024-06-05 15:45:24 +0100

    Sample push testing

--- runAndroidSample.sh
+++ runAndroidSample.sh
@@ -1,11 +1,11 @@
 clear
-jenv global 1.8
+jenv global 17.0.11
 export ANDROID_SDK_ROOT=~/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 cd Example
-cordova plugin remove ../intercom-plugin
+cordova plugin remove cordova-plugin-intercom
 cordova plugin add ../intercom-plugin
 cordova platform remove android
-cordova platform add android@10.1.1 --save
+cordova platform add android@13.0.0 --save
 cordova run android --stacktrace
