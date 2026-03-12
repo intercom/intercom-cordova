84657565299688b7eea0ce0f9877f2a84ddc7651
Irena Szukala <irena.szukala@intercom.consulting>
2026-03-12 11:49:35 +0100

    Release 15.0.1

--- intercom-plugin/src/ios/IntercomBridge.m
+++ intercom-plugin/src/ios/IntercomBridge.m
@@ -16,7 +16,7 @@
 #pragma mark - Intercom Initialisation
 
 - (void)pluginInitialize {
-    [Intercom setCordovaVersion:@"15.0.0"];
+    [Intercom setCordovaVersion:@"15.0.1"];
     #ifdef DEBUG
         [Intercom enableLogging];
     #endif
